1261e139d72031be7f53a3f9667af2e3c640554b
CortezNoahh <noahcortez22@gmail.com>
2024-12-18 04:59:41 +0800

    Add files via upload
    Admission Page done

--- epcst_mockup/resources/views/admission.blade.php
+++ epcst_mockup/resources/views/admission.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body class="d-flex flex-column" style="background-color:#fdf0f0; padding-bottom: 300px">
 
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#afdc76; position: relative;">
+    <nav class="navbar navbar-expand-lg navbar-light mb-4" style="background-color:#afdc76; position: relative;">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('mainpage') }}" style="position: absolute; top: 20%; left: 3%; inline-block; padding: 0; margin: 0;">
                 <img src="{{ asset('images/school_logo.png') }}" class="rounded-pill ps-4 pb-2 pe-1" style="width:55%; height:20%; background-color:#afdc76">
@@ -35,6 +35,46 @@
             </div>
         </div>
     </nav>
+
+    <div class="container d-flex justify-content-center align-items-center mt-5" style="max-width: 1800px; width: 100%;">
+        <div class="d-flex justify-content-around align-items-center" style="width: 100%;">
+
+            <div class="d-flex flex-column justify-content-center align-items-center" 
+                style="background-color: #afdc76; max-width: 48%; width: 40%; min-height: 70px; margin-right: 10px;">
+                <p class="text-center" style="color: #347928; font-size: 30px;">
+                    For New Students
+                </p>
+            </div>
+
+            <div class="d-flex flex-column justify-content-center align-items-center" 
+                style="background-color: #afdc76; max-width: 48%; width: 40%; min-height: 70px; margin-left: 10px;">
+                <p class="text-center" style="color: #347928; font-size: 30px;">
+                    For Old Students
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center align-items-center mt-2" style="max-width: 1800px; width: 100%;">
+        <div class="d-flex justify-content-around align-items-center" style="width: 100%;">
+
+            <div class="d-flex flex-column pt-3" 
+                style="background-color: #afdc76; max-width: 48%; width: 40%; min-height: 600px; margin-right: 10px;">
+                <p class="text-center" style="color: #347928; font-size: 30px;">
+                    Directions
+                </p>
+            </div>
+
+            <div class="d-flex flex-column pt-3" 
+                style="background-color: #afdc76; max-width: 48%; width: 40%; min-height: 600px; margin-left: 10px;">
+                <p class="text-center" style="color: #347928; font-size: 30px;">
+                    Directions
+                </p>
+            </div>
+
+        </div>
+    </div>
 
 </body>
 </html>
